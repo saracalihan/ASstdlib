22574a90c7f686a0dd70cb919e9062a715e6013a
Alihan SARAÇ(Brick) <saracalihan@gmail.com>
2024-06-18 22:14:34 +0300

    feat: variant type updated

--- Variant.h
+++ Variant.h
@@ -1,82 +1,324 @@
-/*
- * Copyright (c) 2023, Alihan SARAÇ <saracalihan@gmail.com>
- *
- * This file is under the "MIT" license. Please read "LICENSE" file for more detail.
- */
+#ifndef VARIANT_H
+#define VARIANT_H
 
-#ifndef VARIANT_HEADER
-#define VARIANT_HEADER
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
-#include <cstdlib>
-#include <cstring>
+typedef enum {
+    TYPE_NULL,
+    TYPE_INT,
+    TYPE_BOOL,
+    TYPE_STRING,
+    TYPE_UNDEFINED,
+} VariantType;
 
-namespace ASstdlib {
+typedef struct {
+    VariantType type;
+    union {
+        int int_value;
+        bool bool_value;
+        char *string_value;
+    };
+} Variant;
 
-enum VARIANT_DATA_TYPES {
-  NULL_TYPE,
-  VOID_TYPE,
-  BOOL_TYPE,
-  CHAR_TYPE,
-  INTAGER_TYPE,
-  SIGNED_INTAGER_TYPE,
-  UNSIGNED_INTAGER_TYPE,
-  FLOAT_TYPE,
-  DOUBLE_TYPE,
-  STRING_TYPE,
-  DATE_TYPE,
-};
-
-char *variant_type_to_string(VARIANT_DATA_TYPES t) {
-  char *name = (char *)malloc(sizeof(char) * 25);
-  memset(name, 0, 25);
-
-  switch (t) {
-  case NULL_TYPE:
-    strncpy(name, "null", 4);
-    break;
-  case VOID_TYPE:
-    strncpy(name, "void", 4);
-    break;
-  case BOOL_TYPE:
-    strncpy(name, "bool", 4);
-    break;
-  case CHAR_TYPE:
-    strncpy(name, "char", 4);
-    break;
-  case INTAGER_TYPE:
-    strncpy(name, "intager", 7);
-    break;
-  case SIGNED_INTAGER_TYPE:
-    strncpy(name, "signed intager", 14);
-    break;
-  case UNSIGNED_INTAGER_TYPE:
-    strncpy(name, "unsigned intager", 16);
-    break;
-  case FLOAT_TYPE:
-    strncpy(name, "float", 5);
-    break;
-  case DOUBLE_TYPE:
-    strncpy(name, "double", 6);
-    break;
-  }
-  return name;
+Variant variant_create_int(int d){
+    return (Variant){
+        .type = TYPE_INT,
+        .int_value = d,
+    };
 }
 
-typedef VARIANT_DATA_TYPES VariantDataTypes;
-
-struct S_VariantData {
-  VariantDataTypes type;
-  union {
-    bool bool_v;
-    char char_v;
-    int int_v; // int, signed int, unsigned int with casting
-    float float_v;
-    double double_v;
-    char *string_v;
-  } value;
-};
-
-typedef S_VariantData VariantData;
-
-#endif
+Variant variant_create_bool(bool d){
+    return (Variant){
+        .type = TYPE_BOOL,
+        .bool_value = d,
+    };
 }
+Variant variant_create_string(char* d){
+    return (Variant){
+        .type = TYPE_STRING,
+        .string_value = d,
+    };
+}
+Variant variant_create_null(bool d){
+    return (Variant){
+        .type = TYPE_NULL,
+    };
+}
+
+char* variant_to_string(const Variant* v){
+    char str[256] = {0};
+
+    switch (v->type){
+        case TYPE_STRING:
+            char *p = v->string_value;
+            while(p){
+                if(*p == '\0'){
+                    break;
+                }
+                // convert newline to real newline
+                if(strncmp(p, "\\n", 2) ==0){
+                    *p = '\n';
+                    memcpy(++p,  p+2, strlen(p -2));
+                    p +=2;
+                }
+                p++;
+            }
+            return v->string_value;
+        break;
+        case TYPE_BOOL:
+            strcpy(str, v->bool_value ? "true" : "false");
+            break;
+        case TYPE_INT:
+            sprintf(str, "%d", v->int_value);
+            break;
+        case TYPE_NULL:
+            sprintf(str, "null");
+            break;
+        case TYPE_UNDEFINED:
+            sprintf(str, "undefined");
+        break;
+    }
+    return strdup(str);
+}
+
+char* variant_to_printf_string(const Variant* v){
+    char *s = variant_to_string(v);
+    if(v->type == TYPE_STRING){
+        size_t len = strlen(s)+3;
+        char* temp = malloc(sizeof(char)*len);
+        memset(temp, 0, sizeof(char)*len);
+        sprintf(temp, "\"%s\"", s);
+        return temp;
+    }
+    return s;
+}
+
+Variant variant_add(Variant a, Variant b){
+    if(a.type != b.type) {
+        printf( "[PLUS OPERATOR]: Can not add %s and %s\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+
+    Variant result;
+
+    result.type = a.type;
+    if (a.type == TYPE_INT) {
+        result.int_value = a.int_value + b.int_value;
+    } else if (a.type == TYPE_STRING) {
+        result.string_value = (char *)malloc(strlen(a.string_value) + strlen(b.string_value) + 1);
+        strcpy(result.string_value, a.string_value);
+        strcat(result.string_value, b.string_value);
+    } else if (a.type == TYPE_BOOL) {
+        result.bool_value = (bool)(a.bool_value | b.bool_value);
+    }   else {
+        printf( "[PLUS OPERATOR]: This type not implemented yet! Type number: %ul\n", a.type);
+        exit(1);
+    }
+
+    return result;
+}
+
+Variant variant_sub(Variant a, Variant b){
+    if(a.type != b.type) {
+             printf( "[SUBTRACTION OPERATOR]: Can not subtract %s and %s\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+
+    Variant result;
+
+    result.type = a.type;
+    if (a.type == TYPE_INT) {
+        result.int_value = a.int_value - b.int_value;
+    } else if (a.type == TYPE_STRING) {
+        printf( "[SUBTRACTION OPERATOR]: Can not subtract string types\n");
+        exit(1);
+    } else if (a.type == TYPE_BOOL) {
+        int r = a.int_value - b.int_value;
+        if(r<0){
+            r =0;
+        }
+        result.bool_value = r;
+    }   else {
+        printf( "[SUBTRACTION OPERATOR]: This type not implemented yet! Type number: %ul\n", a.type);
+        exit(1);
+    }
+
+    return result;
+}
+
+Variant variant_mult(Variant a, Variant b){
+    if(a.type != b.type) {
+        printf( "[MULTIPLY OPERATOR]: Can not multiply %s and %s\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+
+    Variant result;
+
+    result.type = a.type;
+    if (a.type == TYPE_INT) {
+        result.int_value = a.int_value * b.int_value;
+    } else {
+        printf( "[MULTIPLY OPERATOR]: Only intagers can multiply!\n");
+        exit(1);
+    }
+    return result;
+}
+Variant variant_div(Variant a, Variant b){
+        if(a.type != b.type) {
+        printf( "[DIVIDE OPERATOR]: Can not divide %s and %s\n", variant_to_string(&a), variant_to_string(&b));
+
+        exit(1);
+    }
+
+    Variant result;
+
+    result.type = a.type;
+    if (a.type == TYPE_INT) {
+        result.int_value = a.int_value / b.int_value;
+    } else {
+
+        printf( "[DIVIDE OPERATOR]: Only intagers can divide!\n");
+        exit(1);
+    }
+    return result;
+}
+
+bool variant_is_truthy(const Variant a){
+    bool ret = false;
+
+    switch (a.type){
+        case TYPE_BOOL:
+            ret = a.bool_value;
+            break;
+        case TYPE_STRING:
+            ret = strlen(a.string_value) > 0;
+            break;
+        case TYPE_INT:
+            ret = a.int_value > 0;
+            break;
+        case TYPE_NULL:
+            ret = false; // null is always falsy
+            break;
+        default:
+            printf("[TRUTHY OPERATOR] Not implemented yet!\n");
+            exit(1);
+    }
+    return ret;
+}
+
+bool variant_and(const Variant a, const Variant b){
+    if(a.type != b.type){
+        printf("[LOGIC OPERATOR] Can not use && operator with %s and %s!\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+    return variant_is_truthy(a) && variant_is_truthy(b);
+}
+bool variant_or(const Variant a, const Variant b){
+    if(a.type != b.type){
+        printf("[LOGIC OPERATOR] Can not use || with %s and %s!\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+
+    return variant_is_truthy(a) || variant_is_truthy(b);
+}
+bool variant_gt(const Variant a, const Variant b){
+    if(a.type != b.type){
+        printf("[LOGIC OPERATOR] Can not use > operator with %s and %s!\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+    if(a.type == TYPE_INT){
+        return a.int_value > b.int_value;
+    } else if(a.type == TYPE_BOOL){
+        return a.bool_value > b.bool_value;
+    } else{
+        printf("[LOGIC OPERATOR] Can use > operator with %s and %s!\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+}
+bool variant_lt(const Variant a, const Variant b){
+    if(a.type != b.type){
+        printf("[LOGIC OPERATOR] Can not use < operator with %s and %s!\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+    if(a.type == TYPE_INT){
+        return a.int_value < b.int_value;
+    } else if(a.type == TYPE_BOOL){
+        return a.bool_value < b.bool_value;
+    } else{
+        printf("[LOGIC OPERATOR] Can use < operator with %s and %s!\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+}
+
+bool variant_gte(const Variant a, const Variant b){
+    if(a.type != b.type){
+        printf("[LOGIC OPERATOR] Can not use >= operator with %s and %s!\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+    if(a.type == TYPE_INT){
+        return a.int_value >= b.int_value;
+    } else if(a.type == TYPE_BOOL){
+        return a.bool_value >= b.bool_value;
+    } else{
+        printf("[LOGIC OPERATOR] Can use >= operator with %s and %s!\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+}
+
+bool variant_lte(const Variant a, const Variant b){
+    if(a.type != b.type){
+        printf("[LOGIC OPERATOR] Can not use <= operator with %s and %s!\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+    if(a.type == TYPE_INT){
+        return a.int_value <= b.int_value;
+    } else if(a.type == TYPE_BOOL){
+        return a.bool_value <= b.bool_value;
+    } else{
+        printf("[LOGIC OPERATOR] Can use <= operator with %s and %s!\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+}
+bool variant_not(const Variant a){
+    if(a.type != TYPE_STRING){
+        printf("[NOT OPERATOR] Can get oposite of %s!\n", variant_to_string(&a));
+        exit(1);
+    }
+
+    return !variant_is_truthy(a);
+}
+bool variant_eq(const Variant a, const Variant b){
+    if((a.type == TYPE_INT && b.type == TYPE_BOOL) || (a.type == TYPE_BOOL && b.type == TYPE_INT)){ // int can use like bool
+        return variant_is_truthy(a) == variant_is_truthy(b);
+    }
+    if(a.type != b.type){
+        printf("[LOGIC OPERATOR] Can not use == operator with %s and %s!\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+
+    if(a.type == TYPE_STRING){
+        return strcmp(a.string_value, b.string_value) == 0;
+    }
+
+    return a.string_value == b.string_value;
+}
+bool variant_not_eq(const Variant a, const Variant b){
+    if((a.type == TYPE_INT && b.type == TYPE_BOOL) || (a.type == TYPE_BOOL && b.type == TYPE_INT)){ // int can use like bool
+        return variant_is_truthy(a) != variant_is_truthy(b);
+    }
+    if(a.type != b.type){
+        printf("[LOGIC OPERATOR] Can not use != with %s and %s!\n", variant_to_string(&a), variant_to_string(&b));
+        exit(1);
+    }
+
+    if(a.type == TYPE_STRING){
+        return strcmp(a.string_value, b.string_value) != 0;
+    }
+
+    return a.string_value != b.string_value;
+}
+
+#endif // VARIANT_H
